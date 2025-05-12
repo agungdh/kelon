eea681f680954d5a2cde2d8d4cd99a012eb3707d
AgungDH <agungdh@live.com>
2025-05-12 13:17:20 +0700

    Refactor PostResource and CreatePost by removing unused imports and optimizing code structure

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -3,9 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
-use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
-use Filament\Forms;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -13,8 +11,6 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class PostResource extends Resource
 {
--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,8 +5,6 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Pages\Concerns\HasRedirectToIndex;
 use App\Filament\Pages\Concerns\HasSaveCancelActions;
 use App\Filament\Resources\PostResource;
-use Filament\Actions;
-use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePost extends CreateRecord
