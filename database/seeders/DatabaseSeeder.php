b9cc355a098902d09876ece0d19bfad6b58de64b
AgungDH <agungdh@live.com>
2025-05-17 07:44:04 +0700

    Add role assignments in DatabaseSeeder for super_admin, student, and teacher

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $superAdminRole = Role::create([
+            'name' => 'super_admin',
+            'guard_name' => 'web',
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student',
+            'guard_name' => 'web',
+        ]);
+
+        $teacherRole = Role::create([
+            'name' => 'teacher',
+            'guard_name' => 'web',
+        ]);
+
+        User::factory(10)->create()->each(function ($user) use ($studentRole) {;
+            $user->assignRole($studentRole);
+        });
+
+        User::factory(10)->create()->each(function ($user) use ($teacherRole) {;
+            $user->assignRole($teacherRole);
+        });
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ])->assignRole($superAdminRole);
     }
 }
