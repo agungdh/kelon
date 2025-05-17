5e5c5209611a6a9f1480f2a78dbf7300ff1fdee3
AgungDH <agungdh@live.com>
2025-05-17 07:38:22 +0700

    Add FilamentShield integration to prohibit destructive commands in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentShield\FilamentShield;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        FilamentShield::prohibitDestructiveCommands($this->app->isProduction());
     }
 }
